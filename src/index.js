0137dcba2a3b6c762509148dc73879399e16e9aa
raifreire <rai.freire26@gmail.com>
2022-08-03 00:09:48 -0300

    insert create user app.post

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todos: []
+  });
+
+  return response.status(201).json(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
